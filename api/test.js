9b8c892bdd3091044e29b78bbf1598a1e9f38f7a
Twan Box <twanbox@gmail.com>
2020-08-18 10:46:16 +0200

    serverless function with mockdata

--- api/test.js
+++ api/test.js
@@ -1,15 +1,8 @@
-const fs = require('fs');
 const flights = require('./flights-db.json');
 
 exports.handler = async (event, context) => {
-  const flightsData = await fs.readFile(flights, (err, data) => {
-    if (err) throw err;
-    return JSON.parse(data);
-  });
-  console.log('event is ', event);
-  console.log('context is ', context);
   return {
     statusCode: 200,
-    body: flightsData,
+    body: await JSON.stringify(flights),
   };
 };
