19214a32a6c4235c1f447f1624b791e1a51c5d26
Kasia Janiszewska <katarzyna.janiszewska@gmail.com>
2020-08-18 15:41:59 +0200

    client side sing up and login pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import FlightTakeOffIcon from '@material-ui/icons/FlightTakeoff'
+import { Link } from 'react-router-dom';
 
-const Login = () => (
-  <div>
-    <h1>Hello I am login page</h1>
-  </div>
-);
 
-export default Login;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
+export default function Login() {
+  const classes = useStyles();
+  return (
+    <div>
+      <form className={classes.root} noValidate autoComplete="off">
+
+        <TextField
+          required
+          id="outlined-required"
+          label="Email address"
+          variant="outlined"
+        />
+        <TextField
+          required
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+        />
+        <Button
+          variant="contained"
+          color="secondary"
+          className={classes.button}
+          endIcon={<FlightTakeOffIcon />}
+        >
+          Log In
+        </Button>
+      </form>
+      <div>
+        <span>Don't have an account yet?</span>
+        <span>
+          <Link to="/account/signup">
+            Sign Up
+          </Link>
+        </span>
+      </div>
+    </div>
+  )
+}
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import FlightTakeOffIcon from '@material-ui/icons/FlightTakeoff'
+import { Link } from 'react-router-dom';
 
-const Signup = () => (
-  <div>
-    <h1> Hello, this is the signup page.</h1>
-  </div>
-);
 
-export default Signup;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+  return (
+    <div>
+      <form className={classes.root} noValidate autoComplete="off">
+      <TextField
+          required
+          id="outlined-required"
+          label="First Name"
+          variant="outlined"
+        />
+         <TextField
+          required
+          id="outlined-required"
+          label="Last Name"
+          variant="outlined"
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Email address"
+          variant="outlined"
+        />
+        <TextField
+          required
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+        />
+        <Button
+          variant="contained"
+          color="secondary"
+          className={classes.button}
+          endIcon={<FlightTakeOffIcon />}
+        >
+          Sign Up
+        </Button>
+      </form>
+      <div>
+        <span>Already have an account?</span>
+        <span>
+          <Link to="/account/login">
+            Log In
+          </Link>
+        </span>
+      </div>
+    </div>
+  )
+}
