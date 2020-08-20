c29c5afee91cad6feb68fa5355da953e51eb005c
Bianca Munteanu <bianca.m.munteanu@gmail.com>
2020-08-20 13:06:21 +0200

    lambda function for getting the places from api

---
+++ api/places.js
@@ -0,0 +1,51 @@
+const unirest = require("unirest");
+
+async function getPlace(place) {
+  try {
+    const req = unirest("GET", "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/NL/EUR/en-GB/");
+
+    req.query({
+      "query": place
+    });
+
+    req.headers({
+      "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+      "x-rapidapi-key": "258854ec1emsh9df9bc6f220edf4p1590c3jsna1bfe0c1615f",
+      "useQueryString": true
+    });
+
+    req.end(function (res) {
+      return res.body;
+    });
+    const data = await req;
+    return data.body;
+  }
+
+  catch(err) {
+    throw new Error(res.error);
+  }
+}
+
+exports.handler = async function (event, context) {
+  console.log(event.body)
+  try {
+    const places = await getPlace(event.body);
+    if (places !== null) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(places)
+      };
+    } else {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(places)
+      };
+    }
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message })
+    };
+  }
+}
