0acdb32e0e78689741d310dd64b0099b14a410ff
Twan Box <twanbox@gmail.com>
2020-08-20 12:53:48 +0200

    Filterplaces in dropdown & set placeID in state

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,6 +6,7 @@ import {
   KeyboardDatePicker,
 } from '@material-ui/pickers';
 import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import FlightTakeOffIcon from '@material-ui/icons/FlightTakeoff';
@@ -27,6 +28,10 @@ const Search = () => {
   const [departureDate, setDepartureDate] = useState(new Date());
   const [returnDate, setReturnDate] = useState(new Date());
   const [places, setPlaces] = useState([]);
+  const [fromValue, setFromValue] = useState(null);
+  const [fromPlaceId, setFromPlaceId] = useState(null);
+  const [toValue, setToValue] = useState(null);
+  const [toPlaceId, setToPlaceId] = useState(null);
   const classes = useStyles();
 
   const handleDepartureChange = (date) => {
@@ -37,75 +42,108 @@ const Search = () => {
     setReturnDate(date);
   };
 
-  const fetchPlaces = (input) => {
-    const result = ['aaa', 'aaab' ,'aaac', 'aaad'];
+  const fetchPlaces = () => {
+    const result = ['aaa', 'aaab', 'aaac', 'aaad'];
     setPlaces(result);
-  }
+  };
+
+  const placesArray = [];
 
   const filterPlaces = (input) => {
     if (input.length > 3) {
       const filteredPlaces = places.filter((place) => place.includes(input));
       setPlaces(filteredPlaces);
     }
-  }
+  };
+
+  const defaultProps = {
+    options: placesArray,
+    getOptionLabel: (place) => `${place.PlaceName}, ${place.CountryName}`,
+  };
 
   return (
     <div className="search-component">
       <form>
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
           <Grid container justify="space-around">
-            <TextField
-              required
-              id="standard-required"
-              label="From"
-              onChange={(e) => e.target.value.length === 3 
-                ? fetchPlaces(e.target.value) 
-                : filterPlaces(e.target.value)}
-            />
-            <TextField
-              required
-              id="standard-required"
-              label="To"
-            />
+            <div className="search-input-width search-input-padding-middle">
+              <Autocomplete
+                options={defaultProps.options}
+                getOptionLabel={defaultProps.getOptionLabel}
+                id="controlled-demo"
+                value={fromValue}
+                onChange={(event, newValue) => {
+                  setFromValue(newValue);
+                  if (newValue) {
+                    setFromPlaceId(newValue.PlaceId);
+                  }
+                }}
+                renderInput={(params) => (
+                  <TextField {...params} label="From" margin="normal" />
+                )}
+              />
+            </div>
+            <div className="search-input-width">
+              <Autocomplete
+                options={defaultProps.options}
+                getOptionLabel={defaultProps.getOptionLabel}
+                id="controlled-demo"
+                value={toValue}
+                onChange={(event, newValue) => {
+                  setToValue(newValue);
+                  if (newValue) {
+                    setToPlaceId(newValue.PlaceId);
+                  }
+                }}
+                renderInput={(params) => (
+                  <TextField {...params} label="To" margin="normal" />
+                )}
+              />
+            </div>
           </Grid>
           <Grid container justify="space-around">
-            <KeyboardDatePicker
-              disableToolbar
-              variant="inline"
-              format="dd/MM/yyyy"
-              margin="normal"
-              id="date-picker-inline"
-              label="departure"
-              value={departureDate}
-              onChange={handleDepartureChange}
-              KeyboardButtonProps={{
-                'aria-label': 'change date',
-              }}
-            />
-            <KeyboardDatePicker
-              disableToolbar
-              variant="inline"
-              margin="normal"
-              id="date-picker-dialog"
-              label="return"
-              format="dd/MM/yyyy"
-              value={returnDate}
-              onChange={handleReturnChange}
-              KeyboardButtonProps={{
-                'aria-label': 'change date',
-              }}
-            />
-
+            <div className="search-input-width search-input-padding-middle">
+              <KeyboardDatePicker
+                disableToolbar
+                variant="inline"
+                format="dd/MM/yyyy"
+                margin="normal"
+                id="date-picker-inline"
+                label="departure"
+                value={departureDate}
+                onChange={handleDepartureChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change date',
+                }}
+              />
+            </div>
+            <div className="search-input-width">
+              <KeyboardDatePicker
+                disableToolbar
+                variant="inline"
+                margin="normal"
+                id="date-picker-dialog"
+                label="return"
+                format="dd/MM/yyyy"
+                value={returnDate}
+                onChange={handleReturnChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change date',
+                }}
+              />
+            </div>
           </Grid>
-          <Button
-            variant="contained"
-            color="secondary"
-            className={classes.button}
-            endIcon={<FlightTakeOffIcon />}
-            type="submit"
-          >
-            Search Flights
-          </Button>
+          <div className="search-btn">
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              endIcon={<FlightTakeOffIcon />}
+              type="submit"
+            >
+              Search Flights
+            </Button>
+          </div>
         </MuiPickersUtilsProvider>
       </form>
     </div>
