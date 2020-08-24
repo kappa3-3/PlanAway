42fad31b915ea60c3e78fbd3d2c7764f2822306d
Bianca Munteanu <bianca.m.munteanu@gmail.com>
2020-08-24 20:22:04 +0200

    formatted save to trip button

--- src/components/Flight.js
+++ src/components/Flight.js
@@ -1,51 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Tooltip from '@material-ui/core/Tooltip';
 import './Flight.css';
 import FavoriteIcon from '@material-ui/icons/FavoriteBorder';
+import FilledIcon from '@material-ui/icons/Favorite';
 
 const Flight = ({
   carrierIn, isDirect, carrierOut, price, currency,
   departurePlace, arrivalPlace, departureDate, arrivalDate,
-}) => (
-  <div className="flights">
-    <div className="flights-container">
-      <div className="carrier carrier-departure">
-        <h3 className="carrier-name">{carrierIn}</h3>
-        <div className="flight-info">
-          <h2>{departurePlace}</h2>
-          <div className="route-info">
-            <h3>{departureDate}</h3>
-            <hr />
-            <h3 className="isDirect">{isDirect ? 'Direct' : 'Indirect'}</h3>
+}) => {
+  const [toggleButton, setToggleButton] = useState(false);
+  return (
+    <div className="flights">
+      <div className="flights-container">
+        <div className="carrier carrier-departure">
+          <h3 className="carrier-name">{carrierIn}</h3>
+          <div className="flight-info">
+            <h2>{departurePlace}</h2>
+            <div className="route-info">
+              <h3>{departureDate}</h3>
+              <hr />
+              <h3 className="isDirect">{isDirect ? 'Direct' : 'Indirect'}</h3>
+            </div>
+            <h2>{arrivalPlace}</h2>
           </div>
-          <h2>{arrivalPlace}</h2>
+        </div>
+        <hr />
+        <div className="carrier carrier-return">
+          <h3 className="carrier-name">{carrierOut}</h3>
+          <div className="flight-info">
+            <h2>{arrivalPlace}</h2>
+            <div className="route-info">
+              <h3>{arrivalDate}</h3>
+              <hr />
+              <h3 className="isDirect">{isDirect ? 'Direct' : 'Indirect'}</h3>
+            </div>
+            <h2>{departurePlace}</h2>
+          </div>
         </div>
       </div>
-      <hr />
-      <div className="carrier carrier-return">
-        <h3 className="carrier-name">{carrierOut}</h3>
-        <div className="flight-info">
-          <h2>{arrivalPlace}</h2>
-          <div className="route-info">
-            <h3>{arrivalDate}</h3>
-            <hr />
-            <h3 className="isDirect">{isDirect ? 'Direct' : 'Indirect'}</h3>
-          </div>
-          <h2>{departurePlace}</h2>
-        </div>
+      <div className="price">
+        <h1>
+          <span>{currency}</span>
+          {price}
+        </h1>
+        <Tooltip title="Add to trip: Hawai" placement="right">
+          <button type="button" className="button" onClick={() => setToggleButton(!toggleButton)}>
+            { toggleButton ? <FilledIcon style={{ color: '#C71062' }} /> : <FavoriteIcon style={{ color: '#C71062' }} />}
+          </button>
+        </Tooltip>
       </div>
     </div>
-    <div className="price">
-      <h1>
-        <span>{currency}</span>
-        {price}
-      </h1>
-      <button type="button" className="button">
-        <FavoriteIcon style={{ color: '#C71062' }} />
-      </button>
-    </div>
-  </div>
-);
+  );
+};
 
 Flight.propTypes = {
   carrierIn: PropTypes.string.isRequired,
